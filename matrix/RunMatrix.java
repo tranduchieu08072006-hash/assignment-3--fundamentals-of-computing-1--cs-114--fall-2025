395fc098d57acac6c26877bca58add4639dcb6fc
tranduchieu08072006-hash <tranduchieu08072006@gmail.com>
2025-10-29 23:09:09 -0400

    commit 1
    Trial version

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,6 +1,71 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-  public static void main(String[] args) {
+  public static void main(String[] args) { 
+    Scanner scanner = new Scanner(System.in); 
+    System.out.print("Enter an integer: "); 
+    int size = scanner.nextInt(); 
+
+    int half = (size / 2) + 1;  
+    int otherHalf = size - half;
+     
+    if (size % 2 != 0) { 
+        for (int row = 1; row <= half; row ++){ 
+            for (int col = 1; col <= half - row; col++){
+                System.out.print(" ");
+            } 
+
+            for (int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("*");
+            } 
+            System.out.println();
+        }  
+        for (int row = otherHalf ; row > 0; row --){ 
+            for (int col = 1; col <= half - row; col++){
+                System.out.print(" ");
+            } 
+
+            for (int col = 1; col <= row * 2 - 1; col++){
+                System.out.print("*");
+            } 
+            System.out.println();
+        }    
+    }else { 
+        for (int i = 1; i <= size + 1 ; i++) System.out.print(" "); 
+        System.out.println("*"); 
+
+        for (int row = 1; row < half; row++){
+            for (int col = 1; col <= half - row; col++){
+                System.out.print("  ");  
+            } 
+
+            for (int col = 1; col <= row*2; col++){
+                System.out.print("* "); 
+                
+            } 
+
+            System.out.println();
+        } 
+        
+        for (int row = otherHalf; row > 0; row--){
+            for (int col = 1; col <= half - row; col++){
+                System.out.print("  ");  
+            } 
+
+            for (int col = 1; col <= row*2; col++){
+                System.out.print("* "); 
+                
+            } 
+
+            System.out.println(); 
+            }     
+
+        for (int i = 1; i <= size + 1 ; i++){
+                 System.out.print(" "); 
+        }
+                 System.out.println("*");
+    }
+
+    scanner.close();
   }
 }
