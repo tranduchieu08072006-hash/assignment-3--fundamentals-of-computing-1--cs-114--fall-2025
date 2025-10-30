dc01e984fd1d5a4c863d07c0a607456deefa6b9c
tranduchieu08072006-hash <tranduchieu08072006@gmail.com>
2025-10-29 23:24:57 -0400

    change 1

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,71 +1,73 @@
 import java.util.Scanner;
 
 public class Diamond {
-  public static void main(String[] args) {  
-     Scanner scanner = new Scanner(System.in); 
-     System.out.print("Enter an integer: "); 
-     int size = scanner.nextInt(); 
+    public static void main(String[] args) {  
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter an integer: "); 
+        int size = scanner.nextInt(); 
 
-     int half = (size / 2) + 1;  
-     int otherHalf = size - half;
+        int half = (size / 2) + 1;  
+        int otherHalf = size - half;
      
-     if (size % 2 != 0) { 
-         for (int row = 1; row <= half; row ++){ 
-             for (int col = 1; col <= half - row; col++){
-                 System.out.print(" ");
-             } 
+        if (size % 2 != 0) { 
+            for (int row = 1; row <= half; row ++){ 
+                for (int col = 1; col <= half - row; col++){
+                    System.out.print(" ");
+                } 
  
-             for (int col = 1; col <= row * 2 - 1; col++){
-                 System.out.print("*");
-             } 
-             System.out.println();
-         }  
-         for (int row = otherHalf ; row > 0; row --){ 
-             for (int col = 1; col <= half - row; col++){
-                 System.out.print(" ");
-             } 
+                for (int col = 1; col <= row * 2 - 1; col++){
+                    System.out.print("*");
+                } 
+                System.out.println();
+            }  
+            for (int row = otherHalf ; row > 0; row --){ 
+                for (int col = 1; col <= half - row; col++){
+                    System.out.print(" ");
+                } 
 
-             for (int col = 1; col <= row * 2 - 1; col++){
-                 System.out.print("*");
-             } 
-             System.out.println();
-        }    
-     }else { 
-         for (int i = 1; i <= size + 1 ; i++) System.out.print(" "); 
-         System.out.println("*"); 
+                for (int col = 1; col <= row * 2 - 1; col++){
+                    System.out.print("*");
+                } 
+                System.out.println();
+            }    
+        }else { 
+            for (int i = 1; i <= size + 1 ; i++) {
+                System.out.print(" ");  
+            } 
+             System.out.println("*");
 
-         for (int row = 1; row < half; row++){
-             for (int col = 1; col <= half - row; col++){
-                 System.out.print("  ");  
-             } 
+            for (int row = 1; row < half; row++){
+                for (int col = 1; col <= half - row; col++){
+                    System.out.print("  ");  
+                } 
 
-             for (int col = 1; col <= row*2; col++){
-                 System.out.print("* "); 
+                for (int col = 1; col <= row*2; col++){
+                    System.out.print("* "); 
                 
-             } 
+                } 
 
-             System.out.println();
-         } 
+                System.out.println();
+            } 
         
-         for (int row = otherHalf; row > 0; row--){
-             for (int col = 1; col <= half - row; col++){
-                 System.out.print("  ");  
-             } 
+            for (int row = otherHalf; row > 0; row--){
+                for (int col = 1; col <= half - row; col++){
+                    System.out.print("  ");  
+                } 
 
-             for (int col = 1; col <= row*2; col++){
-                 System.out.print("* "); 
+                for (int col = 1; col <= row*2; col++){
+                    System.out.print("* "); 
                 
-             } 
+                } 
 
-             System.out.println(); 
-             }     
+                System.out.println(); 
+            }     
 
-         for (int i = 1; i <= size + 1 ; i++){
+            for (int i = 1; i <= size + 1 ; i++){
                  System.out.print(" "); 
-         }
+            }
                  System.out.println("*");
-     }
+        }
 
      scanner.close();
-  }
+   }
 }
