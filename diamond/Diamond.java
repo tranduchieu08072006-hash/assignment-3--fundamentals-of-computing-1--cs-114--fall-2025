546479a856969e80dcf08d3d78c83d7467c66183
tranduchieu08072006-hash <tranduchieu08072006@gmail.com>
2025-10-31 15:35:55 -0400

    change 4

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -30,30 +30,30 @@ public class Diamond {
             }
 
         }else { 
-            for (int i = 1; i <= size + 1 ; i++) {
+            for (int i = 1; i <= size; i++) {
                 System.out.print(" ");  
             } 
              System.out.println("*");
 
             for (int row = 1; row < half; row++){
-                for (int col = 1; col <= half - row; col++){
+                for (int col = 1; col < half - row; col++){
                     System.out.print("  ");  
                 } 
                 for (int col = 1; col <= row*2; col++){
-                    System.out.print("* ");  
+                    System.out.print(" *");  
                 } 
                 System.out.println();
             } 
             for (int row = otherHalf; row > 0; row--){
-                for (int col = 1; col <= half - row; col++){
+                for (int col = 1; col < half - row; col++){
                     System.out.print("  ");  
                 } 
                 for (int col = 1; col <= row*2; col++){
-                    System.out.print("* ");                
+                    System.out.print(" *");                
                 } 
                 System.out.println(); 
             }     
-            for (int i = 1; i <= size + 1 ; i++){
+            for (int i = 1; i <= size; i++){
                  System.out.print(" "); 
             }
                  System.out.println("*");
